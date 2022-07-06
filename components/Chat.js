58015ca32210c8e5b9efa459b3f1e55bf65b0c00
Etta Baker <etta.r.baker@gmail.com>
2022-07-06 07:34:48 -0400

    Implemented AsyncStorage & NetInfo

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,32 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import {
+  View, 
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  Button,
+  Alert,
+} from 'react-native';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import { getApp, getApps, initializeApp } from 'firebase/app';
-import { onSnapshot, query, orderBy, addDoc, collection, getFirestore } from 'firebase/firestore';
-import { getAuth, initializeAuth, getReactNativePersistence, onAuthStateChanged, signInAnonymously } from 'firebase/auth';
+import {
+  onSnapshot,
+  query,
+  orderBy,
+  addDoc,
+  collection,
+  getFirestore,
+} from 'firebase/firestore';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+  onAuthStateChanged,
+  signInAnonymously,
+} from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import NetInfo from '@react-native-community/netinfo';
+import { async } from '@firebase/util';
 
 export default function Chat(props) {
   // Initialize props passed in from navigator
@@ -15,6 +36,7 @@ export default function Chat(props) {
   // Initialize messages and user uid states
   const [messages, setMessages] = useState([]);
   const [uid, setUid] = useState('');
+  const [isConnected, setIsConnected] = useState();
 
   // Config information
   const firebaseConfig = {
@@ -23,14 +45,14 @@ export default function Chat(props) {
     projectId: 'chatapp-53949',
     storageBucket: 'chatapp-53949.appspot.com',
     messagingSenderId: '399680179484',
-    appId: "1:399680179484:web:cfd5870a173f6ffeb87ed9"
+    appId: '1:399680179484:web:cfd5870a173f6ffeb87ed9',
   };
 
   // Initialize firebase app
   let app;
   let auth;
 
-  // Check if app has been initialized already
+  // Check if app has been initialized already & initialize if not
   if (getApps().length === 0) {
     app = initializeApp(firebaseConfig);
     auth = initializeAuth(app, {
@@ -41,36 +63,83 @@ export default function Chat(props) {
     auth = getAuth(app);
   }
   const db = getFirestore(app);
-  
+
   // Reference to Firestore messages collection
   const referenceChatMessages = collection(db, 'messages');
-  
+
+  // Save messages to AsyncStorage
+  const saveMessagesToStorage = async () => {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(messages));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Get messages from AsyncStorage
+  const getStoredMessages = async () => {
+    let messagesarray = '';
+    try {
+      messagesarray = (await AsyncStorage.getItem('messages')) || [];
+      setMessages(JSON.parse(messagesarray));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Delete messages in storage (for testing)
+  const deleteStoredMessages = async () => {
+    try {
+      await AsyncStorage.removeItem('messages');
+      // setMessages([]);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   // Make page title the name passed in
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, [navigation, name]);
 
   useEffect(() => {
-    // Authenticate anonymous user
-    const authUnsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        signInAnonymously(auth);
+    // Check if online
+    NetInfo.fetch().then((connection) => {
+      if (connection.isConnected) {
+        setIsConnected(true);
+      } else {
+        setIsConnected(false);
       }
-      setUid(user.uid);
-      setMessages([]);
     });
-    // get all messages in time order
-    const messagesQuery = query(referenceChatMessages, orderBy("createdAt", "desc"));
 
-    //Creates event listener for messages updates
-    const unsubscribe = onSnapshot(messagesQuery, onCollectionUpdate);
-    
-    // Stop listening to messages and authentication
-    return () => {
-      unsubscribe();
-      authUnsubscribe();
-    };
-  }, []);
+    if (isConnected) {
+      // Authenticate anonymous user
+      const authUnsubscribe = onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          signInAnonymously(auth);
+        }
+        setUid(user.uid);
+        setMessages([]);
+      });
+      // get all messages in time order
+      const messagesQuery = query(
+        referenceChatMessages,
+        orderBy('createdAt', 'desc')
+      );
+
+      //Creates event listener for messages updates
+      const unsubscribe = onSnapshot(messagesQuery, onCollectionUpdate);
+
+      // Stop listening to messages and authentication
+      return () => {
+        unsubscribe();
+        authUnsubscribe();
+      };
+    } else {
+      // alertOffline();
+      getStoredMessages();
+    }
+  }, [isConnected]); // Use effect listening for change in online status
 
   // Update state of messages when database is updated
   const onCollectionUpdate = (querySnapshot) => {
@@ -85,25 +154,39 @@ export default function Chat(props) {
       });
     });
     setMessages(messagesarray);
+    saveMessagesToStorage(messagesarray);
   };
 
   // Add new message to firestore collection
-  const addMessages = (messages) => {
+  const addMessages = (message) => {
     addDoc(referenceChatMessages, {
-      _id: messages._id,
-      text: messages.text,
-      createdAt: messages.createdAt,
-      user: messages.user,
-    })
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
   };
 
   // Append new message to messages state and call function to add to Firestore collection
   const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
-    );
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
     addMessages(messages[0]);
   }, []);
+
+  
+
+  // Display alert if offline
+  const alertOffline = () => {
+    Alert.alert(
+      'Offline',
+      "You are offline. You can see previously saved messages, but can't send any new ones until you are back online.",
+      [
+        {
+          text: 'OK',
+        },
+      ]
+    );
+  };
 
   // Change color of the message bubbles
   const renderBubble = (props) => {
@@ -122,6 +205,14 @@ export default function Chat(props) {
     );
   };
 
+  // Control input toolbar
+  const renderInputToolbar = (props) => {
+    if (isConnected == false) {
+    } else {
+      return <InputToolbar {...props} />;
+    }
+  };
+
   return (
     <View
       style={{
@@ -129,15 +220,26 @@ export default function Chat(props) {
         backgroundColor: chatBackground,
       }}
     >
+      {isConnected ? null : (
+        <Text style={{color: "red"}}>You are offline.</Text>
+      )}
+      <Button
+        onPress={() => deleteStoredMessages()}
+        title="Clear Saved Messages"
+        color="#848884"
+        accessibilityLabel="Clicking this button clears messages from local storage."
+      />
       <GiftedChat
         renderBubble={renderBubble.bind()}
+        renderInputToolbar={renderInputToolbar.bind()}
         messages={messages}
         onSend={(messages) => onSend(messages)}
-        user={{ 
+        user={{
           _id: uid,
           name: name,
         }}
       />
+    
       {/* If platform is android, make sure to move the UI with the keyboard */}
       {Platform.OS === 'android' ? (
         <KeyboardAvoidingView behavior="height" />
